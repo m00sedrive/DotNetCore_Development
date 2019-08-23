c8bd8dd57888aa3790921733d8623fd43d4bd764
Thomas Bedford <thomas.bedford@ebitsllp.com>
2019-08-23 17:21:00 +0100

    began adding .netcore login with middleware

---
+++ DotNetCore_Efuelling/DotNetCore_Efuelling/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿
+using IdentityServer3.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Web.Providers.Entities;
+
+namespace DotNetCore_Efuelling.Controllers
+{
+    public class UserController : Controller
+    {
+        [Authorize]
+        [Route("[controller]")]
+        public class UserControllers : ControllerBase
+        {
+            private IUserService _userService;
+
+            public UserControllers(IUserService userSevrice)
+            {
+                _userService = userSevrice;
+            }
+
+            [AllowAnonymous]
+            [HttpPost("authenticate")]
+            public async Task<IActionResult> Authenticate([FromBody]User userparam)
+            {
+                var user = await _userService.Authenticate(userparam.UserName, userparam.Password);
+
+                if(user == null)
+                {
+                    return BadRequest(new { massage = "Username or Password is incorrect!" });
+                }
+
+                return Ok(user);
+            }
+
+            [HttpGet]
+            public async Task<IActionResult> GetAll()
+            {
+                var users = await _userService.GetAll();
+                return Ok(users);
+            }
+        }
+    }
+}
