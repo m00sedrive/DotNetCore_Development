62a56d6ca72ab580334f028a5c1946b31afc4cd3
Tommy <bedfordbox@gmail.com>
2020-04-04 17:03:27 +0100

    added plants controller to API proj

---
+++ Portfolio_NetCore_API/Portfolio_NetCore_API/Controllers/PlantsController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Portfolio_NetCore_BL;
+using Portfolio_NetCore_DAL;
+
+namespace Portfolio_NetCore_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlantsController : ControllerBase
+    {
+        private readonly APIDomain_DbContext _context;
+
+        public PlantsController(APIDomain_DbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Plants
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Plant>>> GetPlants()
+        {
+            return await _context.Plants.ToListAsync();
+        }
+
+        // GET: api/Plants/5 
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<Plant>> GetPlant(int id)
+        {
+            var plant = await _context.Plants.FindAsync(id);
+            if (plant == null)
+            {
+                return NotFound();
+            }
+            return plant;
+        }
+
+        // PUT: api/Plants/5 
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPlant(int id, Plant plant)
+        {
+            if (id != plant.PlantId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(plant).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlantExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        // POST: api/Plants 
+        [HttpPost]
+        public async Task<ActionResult<Plant>> PostUser(Plant plant)
+        {
+            _context.Plants.Add(plant);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetPlant", new { id = plant.PlantId }, plant);
+        }
+
+        // DELETE: api/Plants/5 
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Plant>> DeletePlant(int id)
+        {
+            var plant = await _context.Plants.FindAsync(id);
+            if (plant == null)
+            {
+                return NotFound();
+            }
+            _context.Plants.Remove(plant);
+            await _context.SaveChangesAsync();
+            return plant;
+        }
+
+        private bool PlantExists(int id)
+        {
+            return _context.Plants.Any(e => e.PlantId == id);
+        }
+    }
+}
